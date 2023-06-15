94f0bf48688389f782493c83a048b341a7da8f8d
tugceaktepe <tugcekolcu.tk@gmail.com>
2023-06-16 01:45:21 +0300

    add okhttpIdlingResources

--- app/src/androidTest/java/com/aktepetugce/pagingmovieexample/util/MockWebServerDispatcher.kt
+++ app/src/androidTest/java/com/aktepetugce/pagingmovieexample/util/MockWebServerDispatcher.kt
@@ -4,8 +4,9 @@ import com.aktepetugce.pagingmovieexample.util.ApiConstants.API_KEY
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.RecordedRequest
+import java.util.concurrent.TimeUnit
 
-class MockServerDispatcher {
+class MockWebServerDispatcher {
 
     internal inner class RequestDispatcher : Dispatcher() {
         override fun dispatch(request: RecordedRequest): MockResponse {
@@ -13,6 +14,7 @@ class MockServerDispatcher {
                 "/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1" ->
                     MockResponse().setResponseCode(200)
                         .setBody(FileReader.readStringFromFile("movies_response.json"))
+                        .throttleBody(1024, 200L, TimeUnit.MILLISECONDS)
                 else -> MockResponse().setResponseCode(400)
             }
         }
